223e88ffa8651cc26afcb17a63d5dab4269ae3d3
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2023-10-26 07:45:08 +0700

    Custom Tailwind Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/serverless";
-import tailwind from "@astrojs/tailwind"
+import vercel from '@astrojs/vercel/serverless';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: 'server',
   adapter: vercel(),
   integrations: [tailwind()],
 });
